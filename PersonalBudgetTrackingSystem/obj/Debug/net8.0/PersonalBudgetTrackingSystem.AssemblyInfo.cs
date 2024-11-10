6f71c6fd5e1e19e90f070bb66785e429e28bee1f
ArtemkaGoldMan <artemka.omelchenko@gmail.com>
2024-11-10 12:34:44 +0100

    now-ui-dashboard

--- PersonalBudgetTrackingSystem/obj/Debug/net8.0/PersonalBudgetTrackingSystem.AssemblyInfo.cs
+++ PersonalBudgetTrackingSystem/obj/Debug/net8.0/PersonalBudgetTrackingSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PersonalBudgetTrackingSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c339bf63aa4a214b08c250915cc585413a6b3aa9")]
 [assembly: System.Reflection.AssemblyProductAttribute("PersonalBudgetTrackingSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PersonalBudgetTrackingSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
