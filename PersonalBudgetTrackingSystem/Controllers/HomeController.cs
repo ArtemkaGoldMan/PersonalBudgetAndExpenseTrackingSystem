8ec1ed5dffeac52de6e726e4e83917859778a9df
ArtemkaGoldMan <artemka.omelchenko@gmail.com>
2024-11-11 17:45:54 +0100

    chart fro expenses

--- PersonalBudgetTrackingSystem/Controllers/HomeController.cs
+++ PersonalBudgetTrackingSystem/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using PersonalBudgetTrackingSystem.Helper;
 using PersonalBudgetTrackingSystem.Models;
 using System.Diagnostics;
 
@@ -6,6 +7,8 @@ namespace PersonalBudgetTrackingSystem.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly string _expenseFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\Data\expenses.json");
+
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
@@ -28,5 +31,34 @@ namespace PersonalBudgetTrackingSystem.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public JsonResult GetExpenseReportData()
+        {
+            var expenses = JsonFileManager.LoadData<Expense>(_expenseFilePath);
+
+            // Monthly Totals
+            var monthlyTotals = expenses
+                .GroupBy(e => new { e.Date.Year, e.Date.Month })
+                .Select(g => new
+                {
+                    Month = new DateTime(g.Key.Year, g.Key.Month, 1).ToString("yyyy-MM"),
+                    TotalAmount = g.Sum(e => e.Amount)
+                })
+                .OrderBy(m => m.Month)
+                .ToList();
+
+            // Category Totals
+            var categoryTotals = expenses
+                .GroupBy(e => e.Category)
+                .Select(g => new
+                {
+                    Category = g.Key ?? "Uncategorized",  // Default to "Uncategorized" if null
+                    TotalAmount = g.Sum(e => e.Amount)
+                })
+                .ToList();
+
+            return Json(new { monthlyTotals, categoryTotals });
+        }
+
     }
 }
--- PersonalBudgetTrackingSystem/obj/Debug/net8.0/PersonalBudgetTrackingSystem.AssemblyInfo.cs
+++ PersonalBudgetTrackingSystem/obj/Debug/net8.0/PersonalBudgetTrackingSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PersonalBudgetTrackingSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56ca42e97e938c9d249e3a0f6823d674de910468")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92377ba3dd2422cdf7e5bd7f5350ad6c399e7085")]
 [assembly: System.Reflection.AssemblyProductAttribute("PersonalBudgetTrackingSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PersonalBudgetTrackingSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
